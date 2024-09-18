125951e82d1c53b27628e959d18fc9e27fb2e4c0
Ian Kabaka <iankabaka1@gmail.com>
2024-09-18 15:42:10 +0300

    practices with loops

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(arry_input, event_name) {
+    const new_array = []
+    for (let i = 0; i < arry_input.length; i++) {
+
+        const new_message = `Thank you, ${arry_input[i]}, for the wonderful ${event_name} gift!`
+        new_array.push(new_message);
+    }
+    return new_array;
+}
+
+function countDown(positive_int) {
+    while (positive_int >= 0) {
+        console.log(positive_int)
+        positive_int--;
+    }
+}
